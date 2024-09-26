e590fc214c2b4bc6b9090d2c9646bdba1a02b6ad
kevinalfito69 <kevinalfito69@gmail.com>
2024-09-27 01:29:49 +0700

    Add: Hero animation and Favorite & back Button

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -18,77 +18,120 @@ class DetailScreen extends StatelessWidget {
       children: [
         Stack(
           children: [
-            Image.asset(place.imageAsset),
-            Container(
-                margin: const EdgeInsets.only(top: 16.0),
-                child: Text(
-                  place.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 30.0,
-                    fontFamily: 'Staatliches',
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
+            Hero(tag: place.name, child: Image.asset(place.imageAsset)),
+            SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    children: [
-                      const Icon(Icons.calendar_month),
-                      const SizedBox(
-                        height: 8.0,
-                      ),
-                      Text(
-                        place.openDays,
-                        style: informationTextStyle,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      const Icon(Icons.watch_later_outlined),
-                      const SizedBox(
-                        height: 8.0,
-                      ),
-                      Text(place.openTime, style: informationTextStyle)
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      const Icon(Icons.monetization_on_outlined),
-                      const SizedBox(
-                        height: 8.0,
-                      ),
-                      Text(place.ticketPrice, style: informationTextStyle)
-                    ],
-                  )
+                  CircleAvatar(
+                      backgroundColor: Colors.black26,
+                      child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.arrow_back, color: Colors.white,))),
+                  const FavoriteButton()
                 ],
               ),
-            ),
-            Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  place.description,
-                  textAlign: TextAlign.justify,
-                  style: const TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
-                )),
-            SizedBox(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: place.imageUrls.map((url) {
-                  return Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(url)));
-                }).toList(),
-              ),
-            )
+
+            )),
+
           ],
-        ))));
+        ),
+        Container(
+            margin: const EdgeInsets.only(top: 16.0),
+            child: Text(
+              place.name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 30.0,
+                fontFamily: 'Staatliches',
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  const Icon(Icons.calendar_month),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(
+                    place.openDays,
+                    style: informationTextStyle,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.watch_later_outlined),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(place.openTime, style: informationTextStyle)
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.monetization_on_outlined),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(place.ticketPrice, style: informationTextStyle)
+                ],
+              )
+            ],
+          ),
+        ),
+        Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              place.description,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
+            )),
+        SizedBox(
+          height: 150,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: place.imageUrls.map((url) {
+              return Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url)));
+            }).toList(),
+          ),
+        )
+      ],
+    ))));
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({super.key});
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+  return  CircleAvatar(backgroundColor: Colors.black26, child:  IconButton(
+    icon:  Icon(isFavorite ? Icons.favorite : Icons.favorite_border, color: isFavorite ? Colors.pink : Colors.white,),
+    onPressed: () {setState(() {
+      isFavorite = !isFavorite;
+    });},
+  ));
   }
 }
