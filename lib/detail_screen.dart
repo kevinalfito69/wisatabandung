1d309839e9cc7915afd37649c77d20d2a0f4c045
kevinalfito69 <kevinalfito69@gmail.com>
2024-09-26 23:30:33 +0700

    Add: Back button on press

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -10,7 +10,7 @@ class DetailScreen extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return Scaffold(
-        appBar: AppBar(leading: IconButton(onPressed: (){}, tooltip: "Back", icon: const Icon(Icons.arrow_back)) ,title: const Text("Back"),),
+        appBar: AppBar(leading: IconButton(onPressed: (){Navigator.pop(context);}, tooltip: "Back", icon: const Icon(Icons.arrow_back)) ,title: const Text("Back"),),
         body: SafeArea(child:
         SingleChildScrollView(
             child:Column(
@@ -76,7 +76,7 @@ class DetailScreen extends StatelessWidget{
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: [
-                      Padding(padding:const EdgeInsets.all(4.0), child: ClipRRect(borderRadius: BorderRadius.circular(10), child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'))),
+                      Padding(padding:const EdgeInsets.all(4.0), child: ClipRRect(borderRadius: BorderRadius.circular(10), child:Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'))),
                       Padding(padding:const EdgeInsets.all(4.0), child: ClipRRect(borderRadius: BorderRadius.circular(10),child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'))),
                       Padding(padding:const EdgeInsets.all(4.0), child: ClipRRect(borderRadius: BorderRadius.circular(10),child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'))),
                     ],
